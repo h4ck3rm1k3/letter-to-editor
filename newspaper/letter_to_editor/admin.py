47cf8430a8b640e8fd63c3403f38cef60274880b
James Michael DuPont <jamesmikedupont@gmail.com>
2014-03-26 04:26:47 -0500

    adding admin

--- newspaper/letter_to_editor/admin.py
+++ newspaper/letter_to_editor/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from letter_to_editor.models import (
+    CirculationHistory,
+    Company,
+    History,
+    Image,
+    Location,
+    Newspaper,
+    Person,
+    PersonRole,
+    Phone,
+    SocialAccount,
+    Webcache,
+    Webpage,
+    Website,
+    WikipediaPage)
+
+for model in (
+    CirculationHistory,
+    Company,
+    History,
+    Image,
+    Location,
+    Newspaper,
+    Person,
+    PersonRole,
+    Phone,
+    SocialAccount,
+    Webcache,
+    Webpage,
+    Website,
+    WikipediaPage) :
+    admin.site.register(model)
